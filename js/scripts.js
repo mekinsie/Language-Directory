64a9286929191affbcc33b9883a760a811aa7284
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-15 11:37:25 -0800

    Move parseInt functions from business logic to user interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 const add = function(option1, option2, option3, option4, option5, option6, option7, option8, option9, option10, option11, option12, option13, option14, option15, option16, option17) {
-  return option1 + option2 + option3 + option4 + option5 + option6 + option7 + option8 + option9 + option10 + option11 + option12 + option13 + option14 + option15 + option16 + option17)
-});
+  return (option1 + option2 + option3 + option4 + option5 + option6 + option7 + option8 + option9 + option10 + option11 + option12 + option13 + option14 + option15 + option16 + option17)
+};
 
+  
+
+$(document).ready(function() { 
+  $("form").submit(function (event) {
+    event.preventDefault();
     const option1 = parseInt($("#codeyes").val());
     const option2 = parseInt($("#codeno").val());
     const option3 = parseInt($("#petyes").val());
@@ -16,14 +21,10 @@ const add = function(option1, option2, option3, option4, option5, option6, optio
     const option12 = parseInt($("#side").val());
     const option13 = parseInt($("#front").val());
     const option14 = parseInt($("#idontsleep").val());
-    const option15 = parseInt($("#petyes").val());
-    const option16 = parseInt($("#petno").val());
-    const option17 = parseInt($("#codeyes").val());
+    const option15 = parseInt($("#microsoft").val());
+    const option16 = parseInt($("#apple").val());
+    const option17 = parseInt($("#neither").val());
     const result = (add(option1, option2, option3, option4, option5, option6, option7, option8, option9, option10, option11, option12, option13, option14, option15, option16, option17));
-
-$(document).ready(function() { 
-  $("form").submit(function (event) {
-    event.preventDefault();
     if (result <= 4) {
       $(".ruby").show();
       $(".python, .csharp").hide();
@@ -37,4 +38,4 @@ $(document).ready(function() {
       $(".python, .ruby").hide();
     }
   });
-  
+});
