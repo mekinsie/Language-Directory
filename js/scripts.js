cdb5aa5d0eb3d4c7e0fe61c500d2f4cee791fb65
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-15 11:22:33 -0800

    Add all 17 options to add function and create show functions depending on result.

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+const add = function(option1, option2, option3, option4, option5, option6, option7, option8, option9, option10, option11, option12, option13, option14, option15, option16, option17) {
+  return option1 + option2 + option3 + option4 + option5 + option6 + option7 + option8 + option9 + option10 + option11 + option12 + option13 + option14 + option15 + option16 + option17)
+});
+
+    const option1 = parseInt($("#codeyes").val());
+    const option2 = parseInt($("#codeno").val());
+    const option3 = parseInt($("#petyes").val());
+    const option4 = parseInt($("#petno").val());
+    const option5 = parseInt($("#right").val());
+    const option6 = parseInt($("#left").val());
+    const option7 = parseInt($("#both").val());
+    const option8 = parseInt($("#coffeeyes").val());
+    const option9 = parseInt($("#coffeeno").val());
+    const option10 = parseInt($("#coffeesometimes").val());
+    const option11 = parseInt($("#back").val());
+    const option12 = parseInt($("#side").val());
+    const option13 = parseInt($("#front").val());
+    const option14 = parseInt($("#idontsleep").val());
+    const option15 = parseInt($("#petyes").val());
+    const option16 = parseInt($("#petno").val());
+    const option17 = parseInt($("#codeyes").val());
+    const result = (add(option1, option2, option3, option4, option5, option6, option7, option8, option9, option10, option11, option12, option13, option14, option15, option16, option17));
+
+$(document).ready(function() { 
+  $("form").submit(function (event) {
+    event.preventDefault();
+    if (result <= 4) {
+      $(".ruby").show();
+      $(".python, .csharp").hide();
+    }
+    if (result = 5 <= 9) {
+      $(".python").show();
+      $(".ruby, .csharp").hide();
+    }
+    if (result = 10 <= 13) {
+      $(".csharp").show();
+      $(".python, .ruby").hide();
+    }
+  });
+  
