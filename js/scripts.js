b8a88f13a40bd2c79574e9054be4b6279fe71094
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-15 12:18:45 -0800

    Change checked selector to selected in parseInt functions.

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,19 @@ $(document).ready(function() {
     let option2 = parseInt($("input [name=code]:checked","#codeno").val());
     let option3 = parseInt($("input [name=pet]:checked","#petyes").val());
     let option4 = parseInt($("input [name=pet]:checked","#petno").val());
-    let option5 = parseInt($("input [name=hand]:checked","#right").val());
-    let option6 = parseInt($("input [name=hand]:checked","#left").val());
-    let option7 = parseInt($("input [name=hand]:checked", "#both").val());
-    let option8 = parseInt($("input [name=coffee]:checked", "#coffeeyes").val());
-    let option9 = parseInt($("input [name=coffee]:checked","#coffeeno").val());
-    let option10 = parseInt($("input [name=coffee]:checked","#coffeesometimes").val());
-    let option11 = parseInt($("input [name=sleep]:checked", "#back").val());
-    let option12 = parseInt($("input [name=sleep]:checked", "#side").val());
-    let option13 = parseInt($("input [name=sleep]:checked", "#front").val());
-    let option14 = parseInt($("input [name=sleep]:checked", "#idontsleep").val());
-    let option15 = parseInt($("input [name=tech]:checked", "#microsoft").val());
-    let option16 = parseInt($("input [name=tech]:checked", "#apple").val());
-    let option17 = parseInt($("input [name=tech]:checked", "#neither").val());
+    let option5 = parseInt($("option [name=hand]:selected","#right").val());
+    let option6 = parseInt($("option [name=hand]:selected","#left").val());
+    let option7 = parseInt($("option [name=hand]:selected", "#both").val());
+    let option8 = parseInt($("option [name=coffee]:selected", "#coffeeyes").val());
+    let option9 = parseInt($("option [name=coffee]:selected","#coffeeno").val());
+    let option10 = parseInt($("option [name=coffee]:selected","#coffeesometimes").val());
+    let option11 = parseInt($("option [name=sleep]:selected", "#back").val());
+    let option12 = parseInt($("option [name=sleep]:selected", "#side").val());
+    let option13 = parseInt($("option [name=sleep]:selected", "#front").val());
+    let option14 = parseInt($("option [name=sleep]:selected", "#idontsleep").val());
+    let option15 = parseInt($("option [name=tech]:selected", "#microsoft").val());
+    let option16 = parseInt($("option [name=tech]:selected", "#apple").val());
+    let option17 = parseInt($("option [name=tech]:selected", "#neither").val());
     let result = (add(option1, option2, option3, option4, option5, option6, option7, option8, option9, option10, option11, option12, option13, option14, option15, option16, option17));
    
    
